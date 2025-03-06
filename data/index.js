c83ce12da66a2c798dfc78373005faf49c432969
Carlos_Sagastume <care_sagastumea@unicah.edu>
2025-03-06 08:33:32 -0600

    Parciales con Parametros directos y por Contexto

--- data/index.js
+++ data/index.js
@@ -1,17 +1,28 @@
 import { navigation } from "./common/navigation"
 
-export const getPageContext = (pagePath) => {
+import { index as indexPageData} from "./pages/index"
+import { contactus as contactUsPageData } from "./pages/contactus"
+
+
+export const getPageContext = (pagePath)=>{
+    console.log("Page to Load Context:", pagePath)
     const commonVariables = {
         ...navigation
     }
 
     let pageVariables = {};
+    console.log("Page been loaded:", pagePath);
     switch (pagePath) {
-        case 'index.html':
+        case '/index.html':
+            pageVariables = indexPageData
             break;
+        case '/contactus.html':
+            pageVariables = contactUsPageData
     }
-    return {
+    const finalContext = {
         ...commonVariables,
         ...pageVariables
     }
+    console.log("Context: ", JSON.stringify(finalContext, null, 2));
+    return finalContext
 }
